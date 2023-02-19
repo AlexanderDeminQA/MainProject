621271f8c5ed1bb32378d0dae6fe99b58a9f8208
schur <schurick.don@gmail.com>
2023-02-20 01:50:19 +0300

    Adde new test authorization page

--- page/main_page.py
+++ page/main_page.py
@@ -1,5 +1,6 @@
 from .base_page import BasePage
 from .locators import MainPageLocators
+from .locators import LoginPageLocators
 from selenium.webdriver.common.by import By
 
 
@@ -10,3 +11,13 @@ class MainPage(BasePage):
 
     def should_be_login_link(self):
         assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
+
+    def should_be_login_url(self):
+        current_url = self.browser.current_url
+        assert 'login' in current_url, 'The login is present in the link'
+
+    def should_be_login_form(self):
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM),  'The form is present in the page'
+
+    def should_be_register_form(self):
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), 'The registr form is present in the page'
